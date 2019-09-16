dacb15f61f60ab61efd08bd760e7bdb944391aae
Sanjay Patel <makasanas@yahoo.in>
2019-09-16 19:17:48 +0530

    Initial dist subtree commit

---
+++ precache-manifest.dec41fa46d7ecfb35caa9ffe7a2ddb3b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8d298a13e66349f74d34b72072ae66e",
+    "url": "/react-blog-list/static/media/avatar.c8d298a1.png"
+  },
+  {
+    "revision": "ac3f799d5bbaf5196fab15ab8de8431c",
+    "url": "/react-blog-list/static/media/Roboto-Regular.ac3f799d.ttf"
+  },
+  {
+    "revision": "fe13e4170719c2fc586501e777bde143",
+    "url": "/react-blog-list/static/media/Roboto-Medium.fe13e417.ttf"
+  },
+  {
+    "revision": "7b5fb88f12bec8143f00e21bc3222124",
+    "url": "/react-blog-list/static/media/Roboto-Light.7b5fb88f.ttf"
+  },
+  {
+    "revision": "d329cc8b34667f114a95422aaad1b063",
+    "url": "/react-blog-list/static/media/Roboto-Bold.d329cc8b.ttf"
+  },
+  {
+    "revision": "f4c9bb71fa3b6f90b2be",
+    "url": "/react-blog-list/static/js/runtime~main.f4c9bb71.js"
+  },
+  {
+    "revision": "61052099ce93c4e87223",
+    "url": "/react-blog-list/static/js/main.61052099.chunk.js"
+  },
+  {
+    "revision": "4cc48307cc1effdd30f2",
+    "url": "/react-blog-list/static/js/1.4cc48307.chunk.js"
+  },
+  {
+    "revision": "61052099ce93c4e87223",
+    "url": "/react-blog-list/static/css/main.6c13d02f.chunk.css"
+  },
+  {
+    "revision": "ed68eebb698d71325362aa0a572a1ee2",
+    "url": "/react-blog-list/index.html"
+  }
+];
